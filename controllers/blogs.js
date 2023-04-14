38f362fee662daeddf6db95c2955b1c5c74d67c1
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-04-14 23:51:31 +0300

    add authentication to delete blog operation

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -47,6 +47,17 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() !== user.id) {
+        return response.status(401).json({ error: 'user not authorized to delete post' })
+    }
+
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
 })
